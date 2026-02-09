dca0cdbe12466311c699786f495c5e9efd7d2eb8
weijialin001 <weijialin001@51talk.com>
2026-02-09 11:56:15 +0800

    [fix] 修复bug

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { User, Album, AlbumImage, UserAlbumImage } = require('../db');
+const { User, Album, AlbumImage, UserAlbumImage, UserAlbum } = require('../db');
 
 const router = express.Router();
 
